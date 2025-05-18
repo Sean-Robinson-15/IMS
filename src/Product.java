8b9a84737c25f40001e3401ce8cf54b48150d023
Sean Robinson <sean.robinson@stfc.ac.uk>
2025-05-18 18:52:44 +0100

    Created Setters

--- src/Product.java
+++ src/Product.java
@@ -28,4 +28,12 @@ public class Product {
     public double getPrice() {
         return price;
     }
+
+    //Setters
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
