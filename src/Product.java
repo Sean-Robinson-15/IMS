481a73a55debee472530a59d01f233efa698ef84
Sean Robinson <sean.robinson@stfc.ac.uk>
2025-05-18 18:41:18 +0100

    Created constructor, and renamed to correct naming scheme

--- src/Product.java
+++ src/Product.java
@@ -1,6 +1,14 @@
 public class Product {
     private int ID;
     private String name;
-    private int Quantity;
+    private int quantity;
     private double price;
+
+    //Constructor
+    public Product(int ID, String name, int quantity, double price) {
+        this.ID = ID;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
 }
