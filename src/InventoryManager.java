e2ea71b7d11544ee7a810de9669234360bfd6ae4
Sean Robinson <sean.robinson@stfc.ac.uk>
2025-05-18 19:12:29 +0100

    created addItem

--- src/InventoryManager.java
+++ src/InventoryManager.java
@@ -2,4 +2,13 @@ import java.util.*;
 
 public class InventoryManager {
     private Map<String, Product> inventory = new HashMap<>();
+        
+
+    public void addItem(String ID, String name, int quantity, double price) {
+        inventory.put(ID, new Product(ID, name, quantity, price));
+        System.out.println("Item added: " + ID + " " + name);
+    }
+
+
+
 }
