8736c9b04b51b4ade90718f6cc42330573950d18
Sean Robinson <sean.robinson@stfc.ac.uk>
2025-05-18 19:25:49 +0100

    created removeItem

--- src/InventoryManager.java
+++ src/InventoryManager.java
@@ -2,13 +2,18 @@ import java.util.*;
 
 public class InventoryManager {
     private Map<String, Product> inventory = new HashMap<>();
-        
 
     public void addItem(String ID, String name, int quantity, double price) {
         inventory.put(ID, new Product(ID, name, quantity, price));
         System.out.println("Item added: " + ID + " " + name);
     }
 
-
+    public void removeItem(String ID) {
+        if (inventory.remove(ID) != null) {
+            System.out.println("Item Removed : " + ID);
+        } else {
+            System.out.println("Error! Product Code (" + ID +") doesnt exist.");
+        }
+    }
 
 }
