e3a7864772409ab18edde774cb5d67c634e91e9b
Sean Robinson <sean.robinson@stfc.ac.uk>
2025-05-18 17:54:08 +0100

    Create Initial Blank Window

--- src/UI.java
+++ src/UI.java
@@ -1,2 +1,16 @@
-public class UI {
+import javax.swing.*;
+
+public class UI extends JFrame {
+
+    public UI() {
+        //Create Window
+        setTitle("IMS by BNU Industry Solutions LTD");
+        setSize(690,420);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new UI().setVisible(true));
+    }
 }
