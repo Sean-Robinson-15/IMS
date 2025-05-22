695a58a97231aa859a5ff6aa42a42b19b6e23044
Sean Robinson <sean.robinson@stfc.ac.uk>
2025-05-22 11:57:47 +0100

    refactored MainMenuUI into UIManager as its scope has changed

---
+++ src/IMS/UI/UIManager.java
@@ -0,0 +1,81 @@
+package IMS.UI;
+import IMS.Inventory.InventoryManager;
+import javax.swing.*;
+import java.awt.*;
+
+
+public class MainMenuUI extends JFrame {
+    public MainMenuUI(InventoryManager manager){
+        setTitle("IMS by BNU Industry Solutions LTD");
+        setSize(690,420);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocation(900, 500);
+
+        JPanel menuPanel = new JPanel(new GridLayout());
+        JButton homeButton = new JButton("Home");
+        JButton inventoryButton = new JButton("Inventory");
+        JButton ordersButton = new JButton("Orders");
+        JButton suppliersButton = new JButton("Suppliers");
+
+        menuPanel.add(homeButton);
+        menuPanel.add(inventoryButton);
+        menuPanel.add(ordersButton);
+        menuPanel.add(suppliersButton);
+        add(menuPanel, BorderLayout.NORTH);
+
+        homeButton.addActionListener(e -> {
+            getContentPane().removeAll();
+            add(menuPanel, BorderLayout.NORTH);
+            repaint();
+            revalidate();
+        });
+
+        inventoryButton.addActionListener(e -> {
+            InventoryUI inventoryUI = new InventoryUI(manager);
+            switchPanel(inventoryUI, menuPanel);
+        });
+
+        ordersButton.addActionListener(e -> {
+            OrdersUI ordersUI = new OrdersUI(manager);
+            switchPanel(ordersUI, menuPanel);
+        });
+
+        suppliersButton.addActionListener(e -> {
+            SuppliersUI suppliersUI = new SuppliersUI(manager);
+            switchPanel(suppliersUI, menuPanel);
+        });
+    }
+
+    private void switchPanel(JPanel panel, JPanel menuPanel) {
+        String panelName = panel.getClass().getSimpleName();
+        panel.setName(panelName);
+        if (!checkUI(panelName)) {
+            removeUIExcept(panelName);
+            add(panel);
+            add(menuPanel, BorderLayout.NORTH);
+            repaint();
+            revalidate();
+        }
+    }
+
+    private boolean checkUI(String string) {
+        for (Component comp : getContentPane().getComponents()) {
+            String name = comp.getClass().getSimpleName();
+            if (!name.equals("JPanel")) {
+                System.out.println("checking " + name);
+            }
+            if (name.equals(string)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private void removeUIExcept(String string) {
+        for (Component comp : getContentPane().getComponents()) {
+            if (!comp.getClass().getSimpleName().equals(string) && !comp.getClass().getSimpleName().equals("JPanel")) {
+                System.out.println("removing "+comp.getClass().getSimpleName());
+                this.remove(comp);
+            }
+        }
+    }
+}
